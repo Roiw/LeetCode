d7799dc8c2ef9cf55d0a9bbef271006ad1bf8507
Lucas <lucas.martins.s@gmail.com>
2020-03-28 20:59:36 -0700

    feat(C#): 1370

---
+++ C#/1370_IncreasingDecreasingString.cs
@@ -0,0 +1,44 @@
+public class Solution {
+    public string SortString(string s) {
+        
+        // Sort string..bubble sort O(N^2)
+        bool mod = true;
+        List<char> nS = new List<char>(s);
+        while (mod == true) {
+            mod = false;
+            for (int i = 0; i < s.Length -1; i++) {
+                if (nS[i] > nS[i+1]){
+                    mod = true;
+                    char c = nS[i+1];
+                    nS[i+1] = nS[i];
+                    nS[i] = c;
+                }
+            }
+        }
+        
+        // S is sorted..
+        string result = ""; int j = 0;
+        bool pickSmallest = true;
+        while (nS.Count > 0) {
+            if (pickSmallest) {
+                result += nS[0]; nS.RemoveAt(0); // Step 1..
+                j = 0;
+                while ( j < nS.Count && result[result.Length -1] <= nS[j]) {
+                    if (result[result.Length -1] < nS[j]) {result += nS[j]; nS.RemoveAt(j);}
+                    else j++;
+                }
+                pickSmallest = false;
+            }
+            else {
+                result += nS[nS.Count-1]; nS.RemoveAt(nS.Count-1); // Step 1..
+                j = nS.Count-1;
+                while (j >= 0 && result[result.Length-1] >= nS[j]) {
+                    if (result[result.Length -1] > nS[j]) {result += nS[j]; nS.RemoveAt(j);}
+                    j--;
+                }
+                pickSmallest = true;
+            }
+        }
+        return  result;
+    }
+}
