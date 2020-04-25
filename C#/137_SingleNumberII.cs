67587bb0ceea70870fadf4a69190e090a088bd54
Lucas <lucas.martins.s@gmail.com>
2020-04-25 15:10:07 -0700

    feat(C#): 137

--- C#/137_SingleNumberII.cs
+++ C#/137_SingleNumberII.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-
+        HashSet<int> elemsFound = new HashSet<int>();
+        HashSet<int> ans = new HashSet<int>(); 
         for(int i = 0; i < nums.Length; i++){
             if (elemsFound.Add(nums[i])){
                 ans.Add(nums[i]);
