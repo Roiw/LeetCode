1c6438ce6ec0169228ee8b866c54a6510132bce6
Lucas <lucas.martins.s@gmail.com>
2020-04-24 21:41:49 -0700

    style(C#): 146 Indentation

--- C#/146_LRUCache.cs
+++ C#/146_LRUCache.cs
@@ -6,41 +6,41 @@ public class LRUCache {
 	private int _capacity = 0;
     
     public LRUCache(int capacity) {
-		ls = new LinkedList<int>();
-		vals = new Dictionary<int,(int, LinkedListNode<int>)>();
-        _capacity = capacity;
+	    ls = new LinkedList<int>();
+	    vals = new Dictionary<int,(int, LinkedListNode<int>)>();
+	    _capacity = capacity;
     }
     
     public int Get(int key) {
-        if (!vals.ContainsKey(key))
-            return -1;
+	    if (!vals.ContainsKey(key))
+		    return -1;
         
-        (int val, LinkedListNode<int> n) = vals[key];     
-		int k = n.Value;
-        ls.Remove(n);
-		vals[key] = (val, ls.AddLast(k));
-        return val;
+	    (int val, LinkedListNode<int> n) = vals[key];
+	    int k = n.Value;
+	    ls.Remove(n);
+	    vals[key] = (val, ls.AddLast(k));
+	    return val;
     }
     
     public void Put(int key, int value) {
-        if (vals.ContainsKey(key)){
-            (int val, LinkedListNode<int> node) = vals[key]; 
-            int k = node.Value;
-        	ls.Remove(node);
-			vals[key] = (value, ls.AddLast(k));
-            return;
-        }   
+	    if (vals.ContainsKey(key)){
+		(int val, LinkedListNode<int> node) = vals[key]; 
+		int k = node.Value;
+		ls.Remove(node);
+		vals[key] = (value, ls.AddLast(k));
+		return;
+	    }   
             
-        if (_capacity == _count) {
-            int k = ls.First.Value;
-			ls.RemoveFirst();
-            vals.Remove(k);       
-        } else {
-            _count++;
-        }
+	    if (_capacity == _count) {
+		    int k = ls.First.Value;
+		    ls.RemoveFirst();
+		    vals.Remove(k);       
+	    } else {
+		    _count++;
+	    }
         
-        // Add normally..
-        vals[key] = (value, ls.AddLast(key));
+	    // Add normally..
+	    vals[key] = (value, ls.AddLast(key));
     }
 }
 
