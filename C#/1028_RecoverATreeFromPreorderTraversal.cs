19b95c44101aa0425902307172a99667f20be999
Lucas <lucas.martins.s@gmail.com>
2020-05-02 19:24:46 -0700

    feat(C#): 1028

---
+++ C#/1028_RecoverATreeFromPreorderTraversal.cs
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private Queue<(int, int)> _nodes = new Queue<(int,int)>();
+    
+    public TreeNode RecoverFromPreorder(string S) {
+        int index = 0;
+        int initial  = 0;
+        (initial, index) = GetNumber(S, index);       
+        
+        // Map string to Dictionary..
+        while (index < S.Length) {
+            int depth = CountDash(S, index);
+            index += depth;
+            int number = 0;
+            (number, index) = GetNumber(S, index);    
+            _nodes.Enqueue((depth, number));
+        }
+        
+        TreeNode root = new TreeNode();
+        root.val = initial;
+        Navigate(root,0);
+        
+        return root;
+    }
+    
+    private void Navigate(TreeNode curr, int depth) {
+        if (_nodes.Count == 0) return;
+        (int currentDepth,  int number) = _nodes.Peek();
+        
+        // Go left..
+        if (currentDepth > depth){
+             _nodes.Dequeue();
+            curr.left = new TreeNode();
+            curr.left.val = number;
+            Navigate(curr.left, depth + 1);
+        }
+        
+        // Go right..
+        if (_nodes.Count == 0) return;
+        (currentDepth, number) = _nodes.Peek();
+        if (currentDepth > depth){
+            _nodes.Dequeue();
+            curr.right = new TreeNode();
+            curr.right.val = number;
+            Navigate(curr.right, depth + 1);
+        }    
+    }
+    
+    
+    private int CountDash(string S, int index){
+        int dashes = 0;
+        while(S[index] == '-') {
+            dashes++;
+            index++;
+        }
+        return dashes;
+    }
+    
+    private (int, int) GetNumber(string S, int index){
+        string number = "";
+        while(index < S.Length && S[index] >= '0' && S[index] <= '9') {
+            number += S[index];
+            index++;
+        }
+        return (Int32.Parse(number), index);
+    }
+}
--- README.md
+++ README.md
@@ -88,6 +88,7 @@ My implementation of some Leet Code problems, data structures and algorithms.
 | 1007   | [Minimum Domino Rotations For Equal Row](https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/)  | [C#](C%23/1007_MinimumDominoRotationsForEqualRow.cs) |   
 | 1008   | [Construct Binary Search Tree from Preorder Traversal](https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/)  | [C#](C%23/1008_ConstructBinarySearchTreeFromPreorderTraversal.cs) |   
 | 1021   | [Remove Outermost Parenthesis](https://leetcode.com/problems/remove-outermost-parentheses/)  | [C#](C%23/1021_RemoveOutermostParentheses.cs) | 
+| 1028   | [Recover a Tree From Preorder Traversal](https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/)  | [C#](C%23/1028_RecoverATreeFromPreorderTraversal.cs) |
 | 1046   | [Last Stone Weight](https://leetcode.com/problems/last-stone-weight/)  | [C#](C%23/1046_LastStoneWeight.cs) |       
 | 1055   | [Shortest Way to Form String](https://leetcode.com/problems/shortest-way-to-form-string/)  | [C#](C%23/1055_ShortestWayToFormString.cs) |     
 | 1108   | [Defanging An Ip Address](https://leetcode.com/problems/defanging-an-ip-address/)  | [Python](Python/1108_DefangingAnIpAddress.py) |     
