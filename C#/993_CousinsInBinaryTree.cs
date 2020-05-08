09bbee3b76be7a4ae767b8e393452722f230b2ce
Lucas <lucas.martins.s@gmail.com>
2020-05-07 19:16:40 -0700

    feat(C#): 993

---
+++ C#/993_CousinsInBinaryTree.cs
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    
+    public bool IsCousins(TreeNode root, int x, int y) {
+
+        int targetDepth = -1;
+        TreeNode lastDad = null;
+        Queue<(TreeNode, int, TreeNode)> queue = new Queue<(TreeNode, int, TreeNode)>();
+
+        queue.Enqueue((root, 0, root));
+               
+        while (queue.Count > 0) {
+            (TreeNode n, int depth, TreeNode dad) = queue.Dequeue();
+            
+            if (n.val == x || n.val == y){
+                if (targetDepth == -1){
+                    targetDepth = depth;
+                    lastDad = dad;
+                }
+                else if (targetDepth == depth && lastDad != dad )
+                    return true;
+                else
+                    return false;
+            }
+            
+            if (n.left != null) queue.Enqueue((n.left, depth+1, n));
+            if (n.right != null) queue.Enqueue((n.right, depth+1, n));
+        }
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -90,6 +90,7 @@ My implementation of some Leet Code problems, data structures and algorithms.
 | 958   | [Check Completeness of a Binary Tree](https://leetcode.com/problems/check-completeness-of-a-binary-tree/)  | [C#](C%23/958_CheckCompletenessOfABinaryTree.cs) |      
 | 977   | [Squares of a Sorted Array](https://leetcode.com/problems/squares-of-a-sorted-array/)  | [C#](C%23/977_SquaresOfASortedArray.cs) |      
 | 981   | [Time Based Key-Value Store](https://leetcode.com/problems/time-based-key-value-store/)  | [C#](C%23/981_TimeBasedKey-ValueStore.cs) |      
+| 993   | [Cousins in Binary Tree](https://leetcode.com/problems/cousins-in-binary-tree/)  | [C#](C%23/993_CousinsInBinaryTree.cs) |      
 | 1002   | [Find Common Characters](https://leetcode.com/problems/find-common-characters/)  | [C#](C%23/1002_FindCommonCharacters.cs) |   
 | 1007   | [Minimum Domino Rotations For Equal Row](https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/)  | [C#](C%23/1007_MinimumDominoRotationsForEqualRow.cs) |   
 | 1008   | [Construct Binary Search Tree from Preorder Traversal](https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/)  | [C#](C%23/1008_ConstructBinarySearchTreeFromPreorderTraversal.cs) |   
