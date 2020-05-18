ad900c23099b6911ce030b1ef903b21114c02e88
Lucas <lucas.martins.s@gmail.com>
2020-05-17 21:32:23 -0700

    feat(C#):986

--- C#/986_IntervalListIntersections.cs
+++ C#/986_IntervalListIntersections.cs
@@ -8,68 +8,16 @@ public class Solution {
         while (a < A.Length && b < B.Length) {
             int i = 0;
             int j = 0;
-            // Case exact start
-            if (A[a][0] == B[b][0]){
-                i = A[a][0];
-                j = 0;
-                if (A[a][1] > B[b][1]){
-                    j = B[b][1];
+            
+            if (A[a][1] < B[b][0]) a++;
+            else if (B[b][1] < A[a][0]) b++;
+            else {
+                i = Math.Max(A[a][0], B[b][0]);
+                j = Math.Min(A[a][1], B[b][1]);
+                ans.Add(new int[]{i,j});
+                if (j == A[a][1]) a++;
+                else
                     b++;
-                    ans.Add(new int[] {i,j});
-                }
-                else {
-                    j = A[a][1];
-                    a++;
-                    ans.Add(new int[] {i,j});
-                }
-            }
-            // Case A start..
-            else if (A[a][0] > B[b][0]){
-                i = A[a][0];
-                if (A[a][0] < B[b][1]) {
-                    if (A[a][1] < B[b][1]){
-                        j = A[a][1];
-                        a++;
-                    } else if (A[a][1] > B[b][1]){
-                        j = B[b][1];
-                        b++;
-                    } 
-                    else {
-                        j = A[a][1];
-                        a++;
-                        b++;
-                    }
-                    ans.Add(new int[] {i,j});
-                }
-                else if (A[a][0] == B[b][1]){
-                    ans.Add(new int[] {i,i});
-                    b++;
-                } else
-                    b++;
-            }
-            // Case B start..
-            else if (B[b][0] > A[a][0]){
-                i = B[b][0];          
-                if (B[b][0] < A[a][1]){
-                    if (B[b][1] < A[a][1]){
-                        j =  B[b][1];
-                        b++;
-                    } else if (B[b][1] > A[a][1]) {
-                        j = A[a][1];
-                        a++;
-                    }
-                    else {
-                        j = B[b][1];
-                        a++;
-                        b++;
-                    }
-                    ans.Add(new int[] {i,j});
-                }
-                else if (B[b][0] == A[a][1]) {
-                    ans.Add(new int[] {i,i});
-                    a++;
-                }else 
-                    a++;
             }
         }
         return ans.ToArray();
