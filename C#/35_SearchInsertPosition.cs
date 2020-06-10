adbc4f236ddf7cb0ac1bcba1c69e63396380307d
Lucas <lucas.martins.s@gmail.com>
2020-06-10 09:43:45 -0700

    refactor(C#): 35, improved complexity

--- C#/35_SearchInsertPosition.cs
+++ C#/35_SearchInsertPosition.cs
@@ -1,10 +1,17 @@
 public class Solution {
     public int SearchInsert(int[] nums, int target) {
-        if (target < nums[0]) return 0;
-        for (int i = 0; i < nums.Length; i++){
-            if (nums[i] >= target)
-                return i;
-        }
-        return nums.Length;
+        // Using C# BinarySearch library..
+        //int e = Array.BinarySearch(nums, target);
+        //return e < 0? ~e : e;
+        // Using own Binary Search
+        return FindIndex(nums, target, 0, nums.Length-1);
+    }
+
+    private int FindIndex(int[] nums, int target, int min, int max){
+        if (min > max) return min; 
+        int mid = (min + max)/2;
+        if (target == nums[mid]) return mid;
+        if (target > nums[mid]) return FindIndex(nums, target, mid+1, max);
+        else return FindIndex(nums, target, min, mid-1); 
     }
 }
