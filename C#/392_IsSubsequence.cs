4f1e4bbb233c58c8fa1d331640b71923304bc8fd
Lucas <lucas.martins.s@gmail.com>
2020-06-11 08:42:52 -0700

    refactor(C#): N*M to N

--- C#/392_IsSubsequence.cs
+++ C#/392_IsSubsequence.cs
@@ -1,15 +1,12 @@
 public class Solution {
     public bool IsSubsequence(string s, string t) {
-        int [][] aux = new int[t.Length + 1][];
-        aux[0] = new int[s.Length + 1];
-        for (int i = 1; i < t.Length + 1; i++) {
-            aux[i] = new int[s.Length + 1];
-            for (int j = 1; j < s.Length + 1; j++) {
-                aux[i][j] = (s[j-1] == t[i-1]) ? 
-                    aux[i-1][j-1] + 1 : Math.Max(aux[i-1][j], aux[i][j-1]);
-            }
-        }
-        if (aux[t.Length][s.Length] == s.Length) return true;
-        else return false;
+        if (t.Length < s.Length || s == null || t == null) return false;
+        if (s.Length == 0) return true;
+        int aux = 0;
+        for (int i = 0; i < t.Length; i++){
+            if (s[aux] == t[i]) aux++;
+            if (aux == s.Length) return true;
+        }      
+        return false;
     }
 }
