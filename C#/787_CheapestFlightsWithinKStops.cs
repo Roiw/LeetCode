7adc9983eccf3012f0f5e638b04862f826aaa835
Lucas <lucas.martins.s@gmail.com>
2020-06-15 23:22:16 -0700

    feat(C#): 787

--- C#/787_CheapestFlightsWithinKStops.cs
+++ C#/787_CheapestFlightsWithinKStops.cs
@@ -1,44 +1,19 @@
-// WORK IN PROGRESS!!
-public class Solution {
-    private Dictionary<int,List<(int,int)>> _graph;
+// Bellman Ford (V*E)
+public class Solution {    
     public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-        _graph = new Dictionary<int,List<(int,int)>>();
-        foreach (int[] e in flights){
-            _graph.TryAdd(e[0], new List<(int,int)>(){(Int32.MinValue,0)});
-            _graph[e[0]].Add((e[1],e[2]));
-        }
-        return Navigate(src, dst, K-1, 0);
-    }
-    
-    private int Navigate (int current, int dst, int K, int price) {
-        if (K < -1 || !_graph.ContainsKey(current))
-            return -1;
-        
-        int minPrice = price;
-        bool found = false;
-        for (int i = 1; i < _graph[current].Count; i++){
-            (int n , int p) = _graph[current][i];
-            if (n == dst){
-                minPrice = minPrice == price? price+p : Math.Min(minPrice, price + p);
-                found = true;
+        Dictionary<int,List<(int,int)>> graph = new Dictionary<int,List<(int,int)>>();
+        int[] paths = new int[n];
+        for(int i = 0; i < n; i++) {paths[i] = Int32.MaxValue;}
+        paths[src] = 0;
+        for (int i = 0; i <= K; i++) {
+            int[] p = new int[n];
+            Array.Copy(paths,p,n);
+            foreach (int[] e in flights) {
+                int orig = e[0], dest = e[1], price = e[2];
+                p[dest] = paths[orig] == Int32.MaxValue? p[dest] : Math.Min(p[dest], paths[orig] + price);
             }
-            else {
-                if (!_graph.ContainsKey(n)) continue;
-                (int nextPrice, int arrived) = _graph[n][0];
-                if (nextPrice == -1) continue;
-                else if (nextPrice > 0 && arrived < minPrice + p)
-                    minPrice = minPrice == price? nextPrice + p : Math.Min(minPrice, nextPrice + p);
-                else {
-                    int aux = Navigate(n, dst, K-1, minPrice + p);
-                    if (aux > 0){
-                        minPrice = minPrice == price? aux : Math.Min(minPrice, aux);
-                        found = true;
-                    }
-                }
-            }
+            paths = p;
         }
-        _graph[current][0] = !found? (-1,0) : (minPrice,price);
-        return _graph[current][0].Item1;
+        return paths[dst] == Int32.MaxValue? -1 : paths[dst];
     }
-    
 }
