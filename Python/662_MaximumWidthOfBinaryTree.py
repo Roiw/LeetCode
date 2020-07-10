283b6d351f94b8ee57482e2578bac14295a380fe
Lucas <lucas.martins.s@gmail.com>
2020-07-10 00:18:37 -0700

    refactor(Python): 662

--- Python/662_MaximumWidthOfBinaryTree.py
+++ Python/662_MaximumWidthOfBinaryTree.py
@@ -4,32 +4,25 @@
 #         self.val = val
 #         self.left = left
 class Solution:
-    
-    extremes = [[1,1]]
-    widths = [1]
-    def Dive(self, root: TreeNode, height:int, value: int):
-        if height == len(self.widths): 
-            self.extremes.append([value, value])
-            self.widths.append(1)
-        else: 
-            if self.extremes[height][0] < value: 
-                self.extremes[height][0] = value
-            if self.extremes[height][1] > value: self.extremes[height][1] = value
-            self.widths[height] = self.extremes[height][0] - self.extremes[height][1] + 1
-            
-        if root.left is None and root.right is None: return
-        if root.left is not None: self.Dive(root.left, height + 1, value * 2)
-        if root.right is not None: self.Dive(root.right, height + 1, value * 2 - 1)
-        
     def widthOfBinaryTree(self, root: TreeNode) -> int:
         if root is None: return 0
-        if root.left is None and root.right is None: return 1
-        self.widths = [1]
+        self.maxWidth = 1
         self.extremes = [[1,1]]
         
+        def Dive(root: TreeNode, height:int, value: int):
+            if height == len(self.extremes): self.extremes.append([value, value])
+            else: 
+                self.extremes[height][0] = max(self.extremes[height][0], value)
+                self.extremes[height][1] = min(self.extremes[height][1], value)
+                width = self.extremes[height][0] - self.extremes[height][1] + 1
+                self.maxWidth = max(self.maxWidth, width)
+
+            if root.left is not None: Dive(root.left, height + 1, value * 2)
+            if root.right is not None: Dive(root.right, height + 1, value * 2 - 1)
+        
         # Starting DFS
-        if root.left is not None: self.Dive(root.left, 1, 2)
-        if root.right is not None: self.Dive(root.right, 1, 1)
+        if root.left is not None: Dive(root.left, 1, 2)
+        if root.right is not None: Dive(root.right, 1, 1)
             
-        return max(self.widths)      
+        return self.maxWidth  
         
