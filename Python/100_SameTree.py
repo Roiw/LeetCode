334a191ef5a0edb2176fe788c11d57175b68c194
Lucas <lucas.martins.s@gmail.com>
2020-07-13 08:51:19 -0700

    refactor(Python): 100

--- Python/100_SameTree.py
+++ Python/100_SameTree.py
@@ -1,45 +1,16 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-def DFS (p: TreeNode, q:TreeNode):
-    if p.val != q.val:
-        return False
-    
-    if p.left == None and p.right == None and q.right == None and q.left == None:
-        return True
-    
-    b = True
-    
-    ## Check left
-    if p.left != None and q.left != None:
-         b = DFS(p.left, q.left)
-    elif not (p.left == None and q.left == None):
-        return False
-    
-    if b == False:
-        return b
-    
-    ## Check right
-    if p.right != None and q.right != None:
-        b = DFS(p.right, q.right)
-    elif not (p.right == None and q.right == None):
-        return False
-            
-    return b
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
+        
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if p == None and q != None:
-            return False
-        if p != None and q == None:
-            return False
-        if p == None and q == None:
-            return True
+        if p == None and q == None: return True
+        elif p == None and q != None or p != None and q == None: return False
+        elif p.val != q.val: return False
         
-        return DFS(p,q)
-        
-        
+        val = self.isSameTree(p.left,q.left)
+        val = val and self.isSameTree(p.right,q.right)
+        return vals
