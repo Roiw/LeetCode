914e14195842d492f73fab5364c99d8e2df467b2
Lucas <lucas.martins.s@gmail.com>
2020-07-25 00:15:13 -0700

    refactor(Python): 797

--- Python/797_AllPathsFromSourceToTarget.py
+++ Python/797_AllPathsFromSourceToTarget.py
@@ -1,16 +1,11 @@
 class Solution:
-    def DFS(self, graph: List[List[int]], path: List[int], node: int) -> List[List[int]]:
+    def DFS(self, graph: List[List[int]], found: List[List[int]], path: List[int], node: int) -> List[List[int]]:
         path.append(node)
-        if node == len(graph) -1: return [path]
-        if node == None: return []
-        
-        rtn = []
-        for n in graph[node]:
-            p = self.DFS(graph, path[:], n)
-            if len(p) > 0: 
-                rtn = rtn + p            
-        return rtn
+        if node == len(graph) -1: found.append(path)
+        else: 
+            for n in graph[node]: found = self.DFS(graph, found, path[:], n)
+        return found
             
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        return self.DFS(graph, [], 0)
+        return self.DFS(graph, [], [], 0)
         
