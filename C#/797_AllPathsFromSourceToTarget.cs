4b346375bc46ee670484eaf8cdf77d373257bfbe
Lucas <lucas.martins.s@gmail.com>
2020-07-25 00:15:53 -0700

    refactor(C#): 797

--- C#/797_AllPathsFromSourceToTarget.cs
+++ C#/797_AllPathsFromSourceToTarget.cs
@@ -1,25 +1,14 @@
 public class Solution {
-    private int Target;
-    private int[][] Graph;
     public IList<IList<int>> AllPathsSourceTarget(int[][] graph) {
-        Target = graph.Length -1;
-        Graph = graph;
-        IList<IList<int>> rst = new List<IList<int>>();
-        List<int> initial = new List<int> {0};
-        rst = Find(rst, initial, 0);
-        return rst;
+        return Find(graph, new List<IList<int>>(), new List<int> {0}, 0);
     }
     
-    public IList<IList<int>> Find(IList<IList<int>> rst, List<int> curr, int index ) {
-        foreach(int e in Graph[index]) {
+    public IList<IList<int>> Find(int[][] graph, IList<IList<int>> rst, List<int> curr, int index ) {
+        foreach(int e in graph[index]) {
             List<int> aux = new List<int>(curr);
-            if (e == Target) {
-                aux.Add(e);
-                rst.Add(aux);
-            } else {
-                aux.Add(e);
-                rst = Find(rst, aux, e);
-            }
+            aux.Add(e);
+            if (e == graph.Length - 1) rst.Add(aux);
+            else rst = Find(graph, rst, aux, e);
         }
         return rst;
     }
