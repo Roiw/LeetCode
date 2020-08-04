a07aa1143417646b59862de9a4608661015300a3
Lucas <lucas.martins.s@gmail.com>
2020-08-03 22:53:53 -0700

    refactor(Python): 125

--- Python/125_ValidPalindrome.py
+++ Python/125_ValidPalindrome.py
@@ -1,30 +1,28 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
-        
-        if s == "" or len(s) == 1:
-            return True
-        
-        s = s.lower()
-        newString = ""
-        for char in s:
-            if char.isalpha() or char.isdigit():
-                newString = newString + char
-        
-        if len(newString) == 0:
-            return True
-        
-        print(newString)
-        
-        half = int(len(newString)/2) + 1
-        
-        if (len(newString)%2 == 0):
-            int(len(newString)/2)
-        
-        for c in range(0, half, 1):
-            aux = len(newString) -1 - c
-            print("Comparing.." + newString[c] + " and " + newString[aux])
-            if newString[c] != newString[aux]:
+        left, right = 0,len(s)-1
+        while left < right:
+            # Set left to a valid character..
+            leftLetter = s[left].lower()
+            validLowerCase =  leftLetter >= 'a' and leftLetter <= 'z'
+            validNumber =  leftLetter >= '0' and leftLetter <= '9'
+            if not validLowerCase and not validNumber:
+                left += 1
+                continue
+                
+            # Set right to a valid character..
+            rightLetter = s[right].lower()
+            validLowerCase =  rightLetter >= 'a' and rightLetter <= 'z'
+            validNumber =  rightLetter >= '0' and rightLetter <= '9' 
+            if not validLowerCase and not validNumber:
+                right -= 1
+                continue
+            
+            if leftLetter != rightLetter:
                 return False
-                                 
+            
+            right -= 1
+            left += 1
+            
         return True
+        
