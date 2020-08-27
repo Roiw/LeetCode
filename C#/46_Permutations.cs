659dfdb030ed3318c33a7949c70e630b60d0eaf1
Lucas <lucas.martins.s@gmail.com>
2020-08-26 23:34:21 -0700

    refactor(C#): 46

--- C#/46_Permutations.cs
+++ C#/46_Permutations.cs
@@ -1,30 +1,30 @@
 public class Solution {
+    
+    // Avoid clutting headers by creating a global var.
     private List<IList<int>> _ans;
+    
+    // Helper function..
+    private void Swap(int[] nums, int a, int b) {
+        int aux = nums[a];
+        nums[a] = nums[b];
+        nums[b] = aux;
+    }
+    
+    // Generates all the permutations..
+    private void GeneratePermutations(int[] nums, int index) {
+        if (index == nums.Length)
+            _ans.Add(new List<int>(nums));
+        
+        for (int i = index; i < nums.Length; i++) {
+            Swap(nums, i, index); // Swap..
+            GeneratePermutations(nums, index + 1);
+            Swap(nums, i, index); // Unswap..
+        }
+    }
+    
     public IList<IList<int>> Permute(int[] nums) {
         _ans = new List<IList<int>>();
         GeneratePermutations(nums, 0);
         return _ans;
-    }
-    private void GeneratePermutations(int[] nums, int key) {
-        
-        // Base case, found a permutation..
-        if (key == nums.Length) 
-            _ans.Add(nums.ToList());
-        
-        for (int i = key; i < nums.Length; i++){
-            
-            // Perform a change
-            int t = nums[i];
-            nums[i] = nums[key];
-            nums[key] = t;
-            
-            // Advance on the key.
-            GeneratePermutations(nums, key +1);
-            
-            // Revert the change..
-            t = nums[i];
-            nums[i] = nums[key];
-            nums[key] = t;
-        }
     }
 }
