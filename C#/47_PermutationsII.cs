3fdb180eb64f6796197f5c72d84a2b5ef5e6bb2d
Lucas <lucas.martins.s@gmail.com>
2020-08-26 23:47:16 -0700

    refactor(C#): 47

--- C#/47_PermutationsII.cs
+++ C#/47_PermutationsII.cs
@@ -1,34 +1,34 @@
 public class Solution {
-    private List<IList<int>> _ans; 
+    
+    private List<IList<int>> _ans;
+    
+    private void Swap(int[] nums, int a, int b) {
+        int c   = nums[a];
+        nums[a] = nums[b];
+        nums[b] = c;
+    }
+    
+    private void GeneratePermutations(int[] nums, int index) {
+        
+        if (nums.Length == index)
+            _ans.Add(new List<int>(nums));
+        
+        HashSet<int> check = new HashSet<int>();
+        
+        for (int i = index; i < nums.Length; i++) {
+            
+            if (!check.Add(nums[i]))
+                continue;
+            
+            Swap(nums, index, i); // Swap
+            GeneratePermutations(nums, index + 1);
+            Swap(nums, index, i); // Restore
+        }
+        
+    }
     public IList<IList<int>> PermuteUnique(int[] nums) {
         _ans = new List<IList<int>>();
-        FindPermutations(nums, 0);
+        GeneratePermutations(nums, 0);
         return _ans;
     }
-    
-    private void FindPermutations(int[] nums, int index) {
-        
-        if (nums.Length == index) {
-            _ans.Add(nums.ToList());
-            return;
-        }
-        
-        HashSet<int> uniques = new HashSet<int>();
-        
-        for (int i = index; i < nums.Length; i++){
-            if(!uniques.Add(nums[i])) continue;
-            Swap(nums, i, index);
-            FindPermutations(nums, index + 1);
-            Swap(nums, i, index);
-        }
-        
-    }
-    
-    private void Swap(int[] nums, int a, int b){
-        if (a == b)
-            return;
-        int t = nums[a];
-        nums[a] = nums[b];
-        nums[b] = t;
-    }    
 }
