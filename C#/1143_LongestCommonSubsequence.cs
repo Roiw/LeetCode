d9f82a9331ab43a9d9d005a62a1d0857ce7511a9
Lucas <lucas.martins.s@gmail.com>
2020-09-19 00:25:04 -0700

    refactor(C#): 1143

--- C#/1143_LongestCommonSubsequence.cs
+++ C#/1143_LongestCommonSubsequence.cs
@@ -1,3 +1,30 @@
+/*
+
+Logest Common Subsequence (LCS) - Dynamic programming O(N * M)
+
+    - Tabulation:
+        - We create a table that has text1 + 1 as column length and text2 + 1 as row length.
+        - Starting from the top position whe fill each cell with:
+            If text1[col] == text[row] => 1 + Table[row - 1][col - 1]
+            Else Max[table[row-1], table[col-1]]
+        - The max sum is at Table[n][m] (last cell)
+*/
+
+public class Solution {
+    public int LongestCommonSubsequence(string text1, string text2) {
+        int[][] table = new int[text1.Length + 1][];
+        table[0] = new int[text2.Length + 1]; 
+        for (int i = 0; i < text1.Length; i++) {
+            table[i + 1] = new int[text2.Length + 1]; 
+            for (int j = 0; j < text2.Length; j++) {
+                table[i + 1][j + 1] = text1[i] == text2[j] ? table[i][j] + 1 : Math.Max(table[i][j + 1], table[i + 1][j]);
+            }
+        }     
+        return table[text1.Length][text2.Length];
+    }
+}
+
+// Same approach different coding..
 public class Solution {
     
     public int LongestCommonSubsequence(string text1, string text2) {
