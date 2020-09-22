65a246ecdaf61546503cd91e937c9c07d7be3716
Lucas <lucas.martins.s@gmail.com>
2020-09-21 23:29:13 -0700

    refactor(C#): 200

--- C#/200_NumberOfIslands.cs
+++ C#/200_NumberOfIslands.cs
@@ -1,93 +1,47 @@
+/*
+
+    Idea: 
+        - Traverse the matrix.
+        - If element not water or used. BFS starting on element.
+        - On BFS end, islands++.
+
+*/
+
 public class Solution {
-    public int NumIslands(char[][] grid) {
-        // Navigate on the land..
-        // Keep last grid position..
-        // Keep island count..
+    private void BFS(char[][] grid, int pX, int pY) {
         
-        int iCount = 0; // Amount of islands.
+        List<(int x, int y)> directions = new List<(int, int)>() { (1, 0), (-1, 0), (0, -1), (0, 1) };
         
-        HashSet<string> visited = new HashSet<string>();
-        int[][] visited2 = new int[grid.Length][];
-        for (int i = 0; i < grid.Length; i++) {
-            visited2[i] = new int[grid[i].Length];
-        }
+        Queue<(int, int)> toVisit = new Queue<(int, int)>();
+        toVisit.Enqueue((pX, pY));
+        grid[pX][pY] = 'v';
+        
+        while (toVisit.Count > 0) {
+            (int xV, int yV) = toVisit.Dequeue();
             
+            foreach (var d in directions) {
+                int x = xV + d.x;
+                int y = yV + d.y;
+
+                if (x >= 0 && x < grid.Length && y >= 0 && y < grid[x].Length && grid[x][y] == '1') {
+                    toVisit.Enqueue((x, y));
+                    grid[x][y] = 'v'; // mark as visited.
+                }
+            }
+        }
+        
+    }
+    public int NumIslands(char[][] grid) {
+        int count = 0;
         for (int i = 0; i < grid.Length; i++) {
             for (int j = 0; j < grid[i].Length; j++) {
-                if (grid[i][j] == '1' &&  visited2[i][j] != 1) {
-                    visited2 = Navigate(i,j,grid, visited2); // Navigate!
-                    iCount++;
+                if (grid[i][j] == '1') {
+                    count++;
+                    BFS(grid, i, j);
                 }
-                else 
-                    visited2[i][j] = 1;
             }
         }
         
-        return iCount;
-    }
-    
-    public int[][] Navigate(int i, int j, char[][] grid, int[][] visited) {
-        Queue<List<int>> toVisit = new Queue<List<int>>();
-        
-        visited[i][j] = 1;
-        toVisit.Enqueue(new List<int>(){i,j});
-        
-        while(toVisit.Count > 0) {
-            List<int> visiting = toVisit.Dequeue();
-             
-            // Check left..
-            if (visiting[1] > 0) {
-                // If there is grid..
-                if (grid[visiting[0]][visiting[1]-1] == '1') {
-                    // If there is land..
-                    if(visited[visiting[0]][visiting[1]-1] == 0) {
-                        // If not visited
-                        visited[visiting[0]][visiting[1]-1] = 1;
-                        toVisit.Enqueue( new List<int>(){visiting[0], visiting[1]-1});
-                    }
-                }         
-            }
-            
-            // Check right..
-            if (visiting[1] < grid[visiting[0]].Length - 1) {
-                // If there is grid..
-                if (grid[visiting[0]][visiting[1]+1] == '1') {
-                    // If there is land..
-                     if(visited[visiting[0]][visiting[1]+1] == 0) {
-                        // If not visited
-                        visited[visiting[0]][visiting[1]+1] = 1;
-                        toVisit.Enqueue( new List<int>(){visiting[0], visiting[1]+1});
-                    }
-                }   
-            }
-            
-            
-            // Check bottom..
-            if (visiting[0] < grid.Length - 1) {
-                // If there is grid..
-                if (grid[visiting[0] + 1][visiting[1]] == '1') {
-                    // If there is land..
-                     if(visited[visiting[0] + 1][visiting[1]] == 0) {
-                        // If not visited
-                        visited[visiting[0] + 1][visiting[1]] = 1;
-                        toVisit.Enqueue( new List<int>(){visiting[0] + 1, visiting[1]});
-                    }
-                }   
-            }
-            
-            // Check top..
-            if (visiting[0] > 0) {
-                // If there is grid..
-                if (grid[visiting[0] - 1][visiting[1]] == '1') {
-                    // If there is land..
-                    if(visited[visiting[0] - 1][visiting[1]] == 0) {
-                        // If not visited
-                        visited[visiting[0] - 1][visiting[1]] = 1;
-                        toVisit.Enqueue( new List<int>(){visiting[0] - 1, visiting[1]});
-                    }
-                }   
-            }
-        }
-        return visited;
+        return count;
     }
 }
